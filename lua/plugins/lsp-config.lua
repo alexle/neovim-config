c3b3423df21efbfc5ea1012ccea7b3e6757c9c76
alexle <alex.csm@gmail.com>
2024-09-07 13:38:53 -0600

    add initial git signs and fugitive

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,7 +17,10 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
+
       lspconfig.lua_ls.setup({})
+      lspconfig.gopls.setup({})
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
