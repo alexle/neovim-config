ac0abdb475859f4d0972f0fd59974a772490cb1e
alexle <alex.csm@gmail.com>
2024-09-17 14:29:19 -0600

    remap telescope keybinding

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,9 +7,12 @@ return {
     },
     config = function()
       local builtin = require("telescope.builtin")
+
       vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
+      vim.keymap.set("n", "<C-[>", builtin.live_grep, {})
+      vim.keymap.set("n", "<C-]", builtin.lsp_references, {})
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
     end,
   },
   {
