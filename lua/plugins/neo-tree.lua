835898b88092b9f81de22bfdd972e6531362bab0
alexle <alex.csm@gmail.com>
2024-09-18 21:08:02 -0600

    weird, spacing change?

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,23 +1,23 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    local neo_tree = require("neo-tree")
-    neo_tree.setup({
-      filesystem = {
-        filtered_items = {
-          hide_dotfiles = false,
-          hide_by_pattern = {
-            ".git",
-            ".gitignore",
-          }
-        },
-      },
-    })
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		local neo_tree = require("neo-tree")
+		neo_tree.setup({
+			filesystem = {
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_by_pattern = {
+						".git",
+						".gitignore",
+					},
+				},
+			},
+		})
+	end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,55 +1,55 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    config = function()
-      local builtin = require("telescope.builtin")
-      local opts = { noremap = true, silent = true }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			local builtin = require("telescope.builtin")
+			local opts = { noremap = true, silent = true }
 
-      vim.keymap.set("n", "<C-p>", builtin.find_files, opts)
-      vim.keymap.set("n", "<C-g>", builtin.live_grep, opts)
-      vim.keymap.set("n", "<leader>r", builtin.lsp_references, opts)
-      vim.keymap.set("n", "<leader>d", builtin.lsp_definitions, opts)
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, opts)
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, opts)
+			vim.keymap.set("n", "<C-p>", builtin.find_files, opts)
+			vim.keymap.set("n", "<C-g>", builtin.live_grep, opts)
+			vim.keymap.set("n", "<leader>r", builtin.lsp_references, opts)
+			vim.keymap.set("n", "<leader>d", builtin.lsp_definitions, opts)
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, opts)
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, opts)
 
-      local actions = require("telescope.actions")
-      require("telescope").setup({
-        defaults = {
-          mappings = {
-            i = {
-              -- Add to escape once out of live_grep
-              --["<esc>"] = actions.close,
-              ["<C-u>"] = false,
-            },
-          },
-        },
-        pickers = {
-          buffers = {
-            mappings = {
-              i = {
-                ["fc"] = actions.delete_buffer + actions.move_to_top,
-              },
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+			local actions = require("telescope.actions")
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							-- Add to escape once out of live_grep
+							--["<esc>"] = actions.close,
+							["<C-u>"] = false,
+						},
+					},
+				},
+				pickers = {
+					buffers = {
+						mappings = {
+							i = {
+								["fc"] = actions.delete_buffer + actions.move_to_top,
+							},
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
