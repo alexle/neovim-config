c626af8aca4d603aeb2df36f39f7060b7dc064e6
alexle <alex.csm@gmail.com>
2024-09-19 21:55:08 -0600

    replace none-ls with conform formatter

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+
+		config = function()
+			local conform = require("conform")
+
+			conform.setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					go = { "gofmt", "goimports", "gci" },
+					python = { "isort", "black" },
+					javascript = { "prettierd", "prettier", stop_after_first = true },
+					terraform = { "terraform_fmt" },
+					bash = { "shfmt" },
+				},
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				},
+			})
+
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			})
+		end,
+	},
+}
