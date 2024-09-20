3a810e00fead38806fb3f68336c741ed10bc5470
alexle <alex.csm@gmail.com>
2024-09-19 22:03:33 -0600

    minor conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,6 +14,7 @@ return {
 					javascript = { "prettierd", "prettier", stop_after_first = true },
 					terraform = { "terraform_fmt" },
 					bash = { "shfmt" },
+					yaml = { "yamlfix" },
 				},
 				format_on_save = {
 					timeout_ms = 500,
