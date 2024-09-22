881f58a40cf2d8536c8a958d1e331ed9de8f790b
alexle <alex.csm@gmail.com>
2024-09-22 09:11:25 -0600

    one monokai color scheme, in case

---
+++ lua/plugins/colorschema.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000, -- load before all other start plugins
+		config = function()
+			vim.cmd.colorscheme("catppuccin-mocha")
+		end,
+	},
+	{
+		"cpea2506/one_monokai.nvim",
+		config = function()
+			-- vim.cmd.colorscheme("one_monokai")
+		end,
+	},
+}
