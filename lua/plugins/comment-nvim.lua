5e56a473fc19c5cb7c5dae4133ac50279265dbee
Alex Le <alex.csm@gmail.com>
2024-11-03 07:06:56 -0700

    tweak gopls lsp config, add gcc/gbc comments

--- lua/plugins/comment-nvim.lua
+++ lua/plugins/comment-nvim.lua
@@ -5,5 +5,15 @@ return {
 			require("Comment").setup()
 		end,
 		opts = {},
+
+		-- NORMAL MODE
+		--  `gcc` - Toggles the current line using linewise comment
+		-- `gbc` - Toggles the current line using blockwise comment
+		-- `[count]gcc` - Toggles the number of line given as a prefix-count using linewise
+		-- `[count]gbc` - Toggles the number of line given as a prefix-count using blockwise
+		--
+		-- VISUAL MODE
+		-- `gc` - Toggles the region using linewise comment
+		-- `gb` - Toggles the region using blockwise comment
 	},
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,32 +1,66 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({capabilities = capabilities})
-      lspconfig.gopls.setup({capabilities = capabilities})
-      lspconfig.bashls.setup({capabilities = capabilities})
-      lspconfig.pylsp.setup({capabilities = capabilities})
-      lspconfig.sqlls.setup({capabilities = capabilities})
-      lspconfig.terraform_lsp.setup({capabilities = capabilities})
-      lspconfig.terraformls.setup({capabilities = capabilities})
-      lspconfig.html.setup({capabilities = capabilities})
-      lspconfig.css_variables.setup({capabilities = capabilities})
-    end,
-  },
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.bashls.setup({ capabilities = capabilities })
+			lspconfig.pylsp.setup({ capabilities = capabilities })
+			lspconfig.sqlls.setup({ capabilities = capabilities })
+			lspconfig.terraform_lsp.setup({ capabilities = capabilities })
+			lspconfig.terraformls.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.css_variables.setup({ capabilities = capabilities })
+
+			lspconfig.gopls.setup({
+				cmd = { "gopls" },
+				filetypes = { "go", "gomod", "gowork" },
+				root_dir = require("lspconfig.util").root_pattern("go.mod", ".git"),
+				capabilities = capabilities,
+				settings = {
+					gopls = {
+						analyses = {
+							unusedparams = true,
+							nilness = true,
+							shadow = true,
+							unusedwrite = true,
+							fieldalignment = true,
+						},
+						staticcheck = true,
+						codelenses = {
+							generate = true, -- Enable code lens to run `go generate`
+							gc_details = true, -- Show detailed heap diagnostics
+							tidy = true, -- Run `go mod tidy`
+							upgrade_dependency = true, -- Code lens to upgrade dependencies
+						},
+						hints = {
+							assignVariableTypes = true,
+							compositeLiteralFields = true,
+							compositeLiteralTypes = true,
+							constantValues = true,
+							parameterNames = true,
+							rangeVariableTypes = true,
+						},
+					},
+				},
+			})
+
+			-- lspconfig.gopls.setup({ capabilities = capabilities })
+		end,
+	},
 }
