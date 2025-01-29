d008ff689c01c43def0739cd8064794ae1897490
Alex Le <alex.csm@gmail.com>
2025-01-29 16:49:51 -0700

    comment out other color scheme

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -11,22 +11,22 @@ return {
 	{
 		"cpea2506/one_monokai.nvim",
 		name = "one_monokai",
-		config = function()
-			vim.cmd.colorscheme("one_monokai")
-		end,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("one_monokai")
+		-- end,
 	},
 	{
 		"scottmckendry/cyberdream.nvim",
 		name = "cyberdream",
-		config = function()
-			vim.cmd.colorscheme("cyberdream")
-		end,
+		-- config = function()
+		-- 	vim.cmd.colorscheme("cyberdream")
+		-- end,
 	},
 	{
 		"projekt0n/github-nvim-theme",
 		name = "github-theme",
-		config = function()
-			vim.cmd("colorscheme github_dark_default")
-		end,
+		-- config = function()
+		-- 	vim.cmd("colorscheme github_dark_default")
+		-- end,
 	},
 }
