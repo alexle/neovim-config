527ea143cc612c38e4c03d2dfb69f610577b23ec
Alex Le <alex.csm@gmail.com>
2026-02-26 22:51:00 -0700

    move open-recent binding from native vscode keybinding to lua vscode-keymaps

--- lua/core/vscode-keymaps.lua
+++ lua/core/vscode-keymaps.lua
@@ -10,6 +10,7 @@ local function action(cmd)
 end
 
 -- File navigation
+map("n", "<leader>;", action("workbench.action.openRecent"), { desc = "Open recent" })
 map("n", "<leader><space>", action("workbench.action.quickOpen"), { desc = "Find files" })
 map("n", "<leader>,", action("workbench.action.showAllEditors"), { desc = "Buffers" })
 map("n", "<leader>p", action("workbench.action.showCommands"), { desc = "Command palette" })
