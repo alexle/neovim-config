3b460865357ea0818e2b36c4736b78e7addb6c28
Alex Le <alex.csm@gmail.com>
2026-02-27 11:00:56 -0700

    remove redundant format-on-save autocmds and unused float terminal mapping

--- lua/keymap-plugins.lua
+++ lua/keymap-plugins.lua
@@ -4,13 +4,6 @@ local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 if not vim.g.vscode then
-	-- Auto-format on save (LSP)
-	vim.api.nvim_create_autocmd("BufWritePre", {
-		callback = function()
-			vim.lsp.buf.format()
-		end,
-	})
-
 	-- Manage floating windows
 	map("n", "<esc>", function()
 		for _, win in ipairs(vim.api.nvim_list_wins()) do
@@ -19,6 +12,4 @@ if not vim.g.vscode then
 			end
 		end
 	end)
-
-	vim.api.nvim_set_keymap("t", "<C-t>", "<C-\\><C-n>:CFloatTerm<CR>", opts)
 end
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -26,13 +26,6 @@ return {
 					lsp_format = "fallback",
 				},
 			})
-
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				pattern = "*",
-				callback = function(args)
-					require("conform").format({ bufnr = args.buf })
-				end,
-			})
 		end,
 	},
 }
